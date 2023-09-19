6c021378af7d18be80631418c8768db35aaf19b3
RajrupDasid <rajrupofficialid@protonmail.com>
2023-09-19 19:22:33 +0530

    Notes Model Schema
    This file Included Notes Model schema
    
    MSL-1
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ models/Note.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+// AutoIncrement is used to deal with automatic update sequence 
+const AutoIncrement = require('mongoose-sequence')(mongoose)
+const noteSchema = new mongoose.Schema({
+    //Note object | object number 1
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    require:true,
+    ref:"User",
+  }, 
+  title:{
+    type:String,
+    require:true
+  }, 
+  text:{
+    type:String,
+    required:true
+  },
+  completed:{
+    type:Boolean,
+    default:false
+  }
+},
+//time object | object number 2
+{
+    timestamps:true
+}
+)
+
+
+
+notesSchema.plugin(AutoIncerement,{
+    inc_field : 'ticket',
+    id:'ticketNums',
+    start_seq:500,
+})
+
+module.exports = mongoose.model('Note',noteSchema)
